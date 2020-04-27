0a000621ddbc8887a78e0ae041b37c130278c739
sidharth atri <sidharthatri@gmail.com>
2020-04-27 09:37:46 +0530

    Updates

---
+++ precache-manifest.6194b03616eecf5f08178520043df3fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c58fe205006e18494a13b224b562cb5",
+    "url": "/COVID-19_Tracker/index.html"
+  },
+  {
+    "revision": "d39962aaf5977b2c4134",
+    "url": "/COVID-19_Tracker/static/css/main.9c78c2ba.chunk.css"
+  },
+  {
+    "revision": "3a254c519c78509ebc1e",
+    "url": "/COVID-19_Tracker/static/js/2.27a6bfa9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19_Tracker/static/js/2.27a6bfa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39962aaf5977b2c4134",
+    "url": "/COVID-19_Tracker/static/js/main.114f48f1.chunk.js"
+  },
+  {
+    "revision": "cdbcfed38d85e698fb66",
+    "url": "/COVID-19_Tracker/static/js/runtime-main.db053155.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19_Tracker/static/media/image.d7265326.png"
+  }
+]);
