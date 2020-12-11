c1883b19467f5d05908a3d61bc4f5b372d9820b4
sidharth atri <sidharthatri@gmail.com>
2020-12-11 21:08:16 +0530

    footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <div className={styles.line}>
-      <h3 className={styles.developer}>Developed By Sidharth Atri</h3>
+      <h3 className={styles.developer}>Developed By Vanshika & Yoshita</h3>
     </div>
   );
 }
